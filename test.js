d0f978721b6d5e2abc664d4ee1c28cf2ee3d9181
anaaftodi <aftodiana21@stud.ase.ro>
2025-11-05 19:37:14 +0200

    tested variables

--- test.js
+++ test.js
@@ -1,14 +1,27 @@
+testVariables(true);
+
 function testVariables(param){
-   
+    console.log(x);
     if(param===true){
         var x=5;
         let y=6;
         const z=7;
     }
-
     console.log(x);
     console.log(y);
     console.log(z);
 }
 
-testVariables(true);
+const testVariables2=(param)=>{
+    console.log(x);
+    if(param===true){
+        var x=5;
+        let y=6;
+        const z=7;
+    }
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+testVariables2(true);
